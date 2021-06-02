b45c621e555507a4620a57add7ef7d491a0674f7
Felipe de Souza Oliveira Martins <felipeso@hagf.corp>
2021-06-02 15:34:11 -0300

    Renderizando lista de nomes

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header'
 
-export default function App() {
-  return (
-    <View >
-        <Header title="Guia Médico!"/>
-        
-    </View>
-  );
+export default class App extends React.Component {
+    renderList() {
+          const names = [
+            'Steve vai',
+            'Jimi Hendrix',
+            'Chimbinha',
+            'Van Halen'
+          ];
+      const textElements = names.map(name =>{
+        return <Text>{name}</Text>
+
+      })
+      return textElements;
+    }
+  render(){
+    return (
+      <View >
+          <Header title="Guia Médico!"/>
+          {this.renderList()}
+      </View>
+    );
+  }
 };
