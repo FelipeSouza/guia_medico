80c5aab10d55d5c71ee6b419bcebfd247fb363f6
Felipe de Souza Oliveira Martins <felipeso@hagf.corp>
2021-06-02 13:46:10 -0300

    Criando o Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+
+
+const Header = (props)=>(
+    <View>
+        <Text>Este é o nosso Header</Text>
+        <Text>Este é o nosso Header</Text>
+    </View>
+
+ );
+
+ export default Header;
