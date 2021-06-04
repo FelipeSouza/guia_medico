02c555be002556d7111a9af2e3f45bf8374fce9c
Felipe de Souza Oliveira Martins <felipeso@hagf.corp>
2021-06-04 16:05:27 -0300

    Estilizando a lista

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
 
 
 const PeopleList = props =>{
     const { peoples } = props;
     const textElements  = peoples.map(people => {
         const { first }= people.name;
-        return <Text key={first}>{first}</Text>;
-      })  
+        return (
+            <View  key={first} style={ style.line}>
+                 <Text style={style.lineText}>
+                     {first}
+                 </Text>
+            </View>
+        );
+      });  
     return(
-        <View>
+        <View style={style.container}>
            { textElements }
         </View>
     )
 };
 
+/*StyleSheet*/
+const style= StyleSheet.create({
+   container:{
+       backgroundColor:'#E2F9FF'
+   },
+  
+   line:{
+    height:60,
+    borderBottomWidth: 1,
+    borderBottomColor: '#bbb',
+    flexDirection: 'row',
+    alignItems: 'center',
+   },
+
+   lineText:{
+        fontSize: 20,
+        paddingLeft: 15,
+    
+   }
+});
 export default PeopleList;
